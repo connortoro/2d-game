a6b0d57409aa3639a4ebed770a5375a3ae1171bf
Connor Toro <connortoro@Connors-Air-2.lan>
2025-03-17 16:13:49 -0700

    Added doors and floor generation

--- room.py
+++ room.py
@@ -6,6 +6,7 @@ class Room:
     empty_grid = None 
     scale = 100
     tile_size = 16
+
     tile_key = {
         'tl': (0, 0),
         't': (1, 0),
@@ -17,6 +18,20 @@ class Room:
         'o': (1, 1),
         'b': (1, 4),
         'x': (2, 3)
+    }
+
+    door_key = {
+        'N': (7, 0),
+        'S': (7, 0),
+        'E': (9, 0),
+        'W': (9, 0)
+    }
+
+    door_dest = {
+        'N': (6, -.2),
+        'S': (6, 6.7),
+        'E': (12.6, 3.3),
+        'W': (.25, 3.3)
     }
 
     # Initializes floor texture and empty grid once
@@ -36,11 +51,12 @@ class Room:
                 ["bl", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "br"]
             ]
 
-    # Init
-    def __init__(self, obstacles_path, enemies_path):
+    def __init__(self, obstacles_path, enemies_path, door_string):
         if Room.texture is None:
-            Room.initialize()  # Ensure initialization if not already done
+            Room.initialize()
 
+        self.door_string = door_string
+        self.doors = []
         self.rectangles = []
         self.object_grid = []
         with open(obstacles_path) as file:
@@ -61,8 +77,11 @@ class Room:
 
     def draw(self):
         self.draw_empty_room()
+        self.draw_doors()
         self.draw_objects()
         self.draw_enemies()
+        for door in self.doors:
+            draw_rectangle_lines_ex(door, 2, RED)
       
     def draw_empty_room(self):
         for dest_y, row in enumerate(self.empty_grid):
@@ -84,6 +103,15 @@ class Room:
     def draw_enemies(self):
         for enemy in self.enemies:
             enemy.draw()
+
+    def draw_doors(self):
+        for dir in self.door_string:
+            source_x, source_y = self.door_key[dir]
+            dest_x, dest_y = self.door_dest[dir]
+
+            source_rect = Rectangle(source_x * self.tile_size, source_y * self.tile_size, self.tile_size, self.tile_size)
+            dest_rect = Rectangle(dest_x * self.scale, dest_y * self.scale,self.scale, self.scale)
+            draw_texture_pro(self.texture, source_rect, dest_rect, Vector2(0,0), 0, WHITE)
           
     def gen_rectangles(self):
         for y, row in enumerate(self.object_grid):
@@ -91,6 +119,13 @@ class Room:
                 if tile_char == 'x':
                     rect = Rectangle(x * self.scale, y * self.scale, self.scale, self.scale)
                     self.rectangles.append(rect)
+        for dir in self.door_string:
+            x, y = self.door_dest[dir]
+            if dir == "N" or dir == "S":
+                x += .3
+                self.doors.append(Rectangle(x*self.scale - 10, y*self.scale + 30, 40, 40))
+            else:
+                self.doors.append(Rectangle(x*self.scale - 10, y*self.scale + 30, 40, 80))
 
     def gen_enemies(self):
         for y, row in enumerate(self.enemy_grid):
